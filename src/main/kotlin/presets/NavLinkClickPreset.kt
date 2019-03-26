a18d92586a8b5459b4ca6105074dd80b71ca2cc1
ETSilence <khlopkovdd@gmail.com>
2019-03-26 17:17:15 +0300

    added presets for getting main page, and for testing navbar

--- src/main/kotlin/presets/NavLinkClickPreset.kt
+++ src/main/kotlin/presets/NavLinkClickPreset.kt
@@ -1,2 +1,22 @@
 package presets
 
+import core.actions.Click
+import core.elements.ElementByXpath
+import core.test.TestPreset
+
+public enum class NavBarLink(val index: Int) {
+    Staff(0),
+    Achivements(1),
+    News(2),
+    Projects(3),
+    Files(4),
+    Media(5),
+    Info(6),
+    Partners(7)
+}
+
+public class NavLinkClickPreset(private val link: NavBarLink): TestPreset(HomePagePreset()) {
+    init {
+        this.addAction(Click(ElementByXpath("//nav[contains(@class, 'navbar')]//a[${this.link.index}]")))
+    }
+}
