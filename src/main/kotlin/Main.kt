075afa5203cd97856b48a59bfd9ab5a1f57b3160
ETSilence <khlopkovdd@gmail.com>
2019-03-26 17:47:30 +0300

    mda

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,34 @@ import core.actions.Click
 import core.actions.DriverAction
 import core.actions.GetUrl
 import core.elements.ElementByXpath
+import core.shoulds.ElementShould
+import core.shoulds.ElementShouldExist
 import core.test.TestCase
+import presets.NavBarLink
+import presets.NavLinkClickPreset
 
 fun main(args: Array<String>) {
-    var testCase = TestCase("test1")
-    testCase.addPreaction(GetUrl("https://student.ifmo.ru"))
-    testCase.addAction(Click(ElementByXpath("//nav[contains(@class, 'navbar')]//a[1]")))
-    println(testCase.runTest().message)
+    testNabarStaffLink()
+}
+
+private val PageHeaderXPath: String = "//h2[contains(@class, 'page-header')]"
+private val PageSubHeaderXPath: String = "//h2[contains(@class, 'page-sub-header')]"
+
+fun testNabarStaffLink() {
+    val testCase = TestCase("test1", NavLinkClickPreset(NavBarLink.Staff))
+
+    testCase.addExpectation(ElementShouldExist(ElementByXpath(PageHeaderXPath)))
+    testCase.addExpectation(ElementShould(
+            "содержать слово Состав",
+            ElementByXpath(PageHeaderXPath)
+    ) { it.text == "Состав" })
+
+    testCase.addExpectation(ElementShouldExist(ElementByXpath(PageHeaderXPath)))
+    testCase.addExpectation(ElementShould(
+            "содержать совет обучающихся",
+            ElementByXpath(PageSubHeaderXPath)
+    ) { it.text == "совет обучающихся" })
+
+    val message = testCase.runTest().message
+    println(message)
 }
--- src/main/kotlin/presets/NavLinkClickPreset.kt
+++ src/main/kotlin/presets/NavLinkClickPreset.kt
@@ -4,19 +4,19 @@ import core.actions.Click
 import core.elements.ElementByXpath
 import core.test.TestPreset
 
-public enum class NavBarLink(val index: Int) {
-    Staff(0),
-    Achivements(1),
-    News(2),
-    Projects(3),
-    Files(4),
-    Media(5),
-    Info(6),
-    Partners(7)
+enum class NavBarLink(val index: Int) {
+    Staff(1),
+    Achievements(2),
+    News(3),
+    Projects(4),
+    Files(5),
+    Media(6),
+    Info(7),
+    Partners(8)
 }
 
-public class NavLinkClickPreset(private val link: NavBarLink): TestPreset(HomePagePreset()) {
+class NavLinkClickPreset(private val link: NavBarLink): TestPreset(HomePagePreset()) {
     init {
-        this.addAction(Click(ElementByXpath("//nav[contains(@class, 'navbar')]//a[${this.link.index}]")))
+        this.addAction(Click(ElementByXpath("/html/body/header/div/div/div[2]/nav/ul/li[${this.link.index}]/a")))
     }
 }
