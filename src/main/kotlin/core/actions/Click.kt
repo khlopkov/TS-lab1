82695a3ff04bcb753434cc8d4ce4cafad37c1d57
ETSilence <khlopkovdd@gmail.com>
2019-03-26 17:42:37 +0300

    throwing exception about not found element when find elements.

--- src/main/kotlin/core/actions/Click.kt
+++ src/main/kotlin/core/actions/Click.kt
@@ -2,7 +2,6 @@ package core.actions
 
 import core.elements.ElementFinder
 import core.elements.ElementQuery
-import core.exceptions.ElementNotFoundException
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 
@@ -13,12 +12,7 @@ class Click constructor(private val query: ElementQuery) : Actionable {
     get() = "click on element with ${this.query.identifierType} \"${this.query.identifier}\""
 
   override fun Do(driver: WebDriver) {
-    try {
-      var element: WebElement = this.elementFinder.findElement(driver)
-      element.click()
-    }
-    catch (e: NoSuchElementException) {
-      throw ElementNotFoundException(this.query)
-    }
+    val element: WebElement = this.elementFinder.findElement(driver)
+    element.click()
   }
 }
--- src/main/kotlin/core/shoulds/ElementShould.kt
+++ src/main/kotlin/core/shoulds/ElementShould.kt
@@ -6,7 +6,7 @@ import core.exceptions.ElementNotFoundException
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 
-public open class ElementShould(
+open class ElementShould(
         protected val what: String,
         private val query: ElementQuery,
         private val elementShould: (WebElement) -> Boolean
@@ -15,12 +15,7 @@ public open class ElementShould(
         get() = "Element with ${this.query.identifierType} \"${this.query.identifier}\" should ${this.what}"
 
     protected fun findElement(driver: WebDriver): WebElement {
-        try {
-            return ElementFinder(this.query).findElement(driver)
-        }
-        catch (e: NoSuchElementException) {
-            throw ElementNotFoundException(this.query)
-        }
+        return ElementFinder(this.query).findElement(driver)
     }
 
     override fun should(driver: WebDriver): Boolean {
