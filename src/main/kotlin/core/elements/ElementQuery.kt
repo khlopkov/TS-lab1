878a81b662e120722ad189333fdde145bfa03eff
khlopkov <khlopkovdd@gmail.com>
2019-04-23 10:53:59 +0300

    Added query element by css selector

--- src/main/kotlin/core/elements/ElementQuery.kt
+++ src/main/kotlin/core/elements/ElementQuery.kt
@@ -3,21 +3,26 @@ package core.elements
 import org.openqa.selenium.By
 
 abstract class ElementQuery constructor(val identifier: String, val by: By) {
-    abstract val identifierType: String;
+    abstract val identifierType: String
 }
 
-public class ElementById constructor(val id: String) : ElementQuery(id, By.id(id)) {
+class ElementById constructor(id: String) : ElementQuery(id, By.id(id)) {
     override val identifierType: String
         get() = "id"
 }
 
-public class ElementByClass constructor(val className: String) : ElementQuery(className, By.className(className)) {
+class ElementByClass constructor(className: String) : ElementQuery(className, By.className(className)) {
     override val identifierType: String
         get() = "className"
 }
 
-public class ElementByXpath constructor(val xpath: String) : ElementQuery(xpath, By.xpath(xpath)) {
+class ElementByXpath constructor(xpath: String) : ElementQuery(xpath, By.xpath(xpath)) {
     override val identifierType: String
         get() = "xpath"
+}
+
+class ElementBySelector constructor(cssSelector: String) : ElementQuery(cssSelector,  By.cssSelector(cssSelector)) {
+    override val identifierType: String
+        get() = "cssSelector"
 }
 
