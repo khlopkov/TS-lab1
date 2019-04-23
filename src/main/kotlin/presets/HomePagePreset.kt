c1bc9c8b8ccaf9388913b29f70c201bc2db9ba52
khlopkov <khlopkovdd@gmail.com>
2019-04-23 10:47:42 +0300

    Removed redundant public modifier

--- src/main/kotlin/presets/HomePagePreset.kt
+++ src/main/kotlin/presets/HomePagePreset.kt
@@ -3,7 +3,7 @@ package presets
 import core.actions.GetUrl
 import core.test.TestPreset
 
-public class HomePagePreset : TestPreset() {
+class HomePagePreset : TestPreset() {
     init {
         this.addPreaction(GetUrl("https://student.ifmo.ru/"))
     }
